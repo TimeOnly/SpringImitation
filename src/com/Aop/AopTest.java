4997e056b3c61d2b2b9d0f5aed1d2819ada37a4d
wangchuangbiao <wangchuangbiao@shsnc.com>
2018-12-20 23:01:57 +0800

    测试在idea上提交东西

--- src/com/Aop/AopTest.java
+++ src/com/Aop/AopTest.java
@@ -2,7 +2,7 @@ package com.Aop;
 
 public class AopTest {
  public void Before(){
-	 System.out.println("Before被调用");
+	 System.out.println("Before被调用,加了一点东西");
  }
  
  public void After(){
---
+++ src/com/Aop/GitTest.java
@@ -0,0 +1,4 @@
+package com.Aop;
+
+public class GitTest {
+}
